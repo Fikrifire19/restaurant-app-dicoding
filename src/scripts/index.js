4d8946ae5557ef35ad794476c432c2fbef1e5424
fikri <muhamadfirki64@yahoo.co.id>
2021-05-21 10:17:37 +0700

    initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,48 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import data from '../data/DATA.json';
+
+const hamburgerButtonElement = document.querySelector("#hamburger");
+const drawerElement = document.querySelector("#drawer");
+const mainElement = document.querySelector("main"); 
+
+hamburgerButtonElement.addEventListener("click", event => {
+    drawerElement.classList.toggle("open");
+    event.stopPropagation();
+});
+
+mainElement.addEventListener("click", event => {
+    drawerElement.classList.remove("open");
+    event.stopPropagation();
+});
+
+// fetch('../data/DATA.json')
+//     .then((data) => data.json())
+//     .then((restaurants) => getData(restaurants));
+console.log(data.restaurants);
+
+// let restaurantList = document.querySelector("#restaurant-card");
+
+const getData = (data) => {
+    let restaurantList = '';
+
+    data.restaurants.forEach((restaurant) => {
+        restaurantList += `
+            <article class="restaurant-item">
+                <div class="restaurant-item-image">
+                    <button id="button-city">${restaurant.city}</button>
+                    <img class="restaurant-item-thumbmail" src=${restaurant.pictureId} alt="gambar-restaurant">
+                </div>
+                <div class="restaurant-item-content">
+                    <p class="restaurant-rating">Rating : ${restaurant.rating}</p>
+                    <h2 class="restaurant-name">${restaurant.name}</h2>
+                    <p class="description">${restaurant.description}</p>
+                </div>
+            </article>
+        `
+
+        document.getElementById("restaurant-list").innerHTML += restaurantList;
+    });
+};
+
+getData(data);
